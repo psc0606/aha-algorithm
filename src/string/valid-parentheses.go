2fdea4dc611f00c2c3cbf7c309db826149666585
pengshaocheng <psc0606@163.com>
2020-12-03 08:25:16 +0800

    solved https://leetcode-cn.com/problems/valid-parentheses/

---
+++ src/string/valid-parentheses.go
@@ -0,0 +1,55 @@
+package string
+
+// https://leetcode-cn.com/problems/valid-parentheses/
+// First:
+// Time: O(n)
+// Space: O(n)
+func isValid(s string) bool {
+	if len(s) == 0 || len(s)%2 == 1 {
+		return false
+	}
+	var stack []int32
+	for _, c := range s {
+		if isLeftBracket(c) {
+			stack = append(stack, c)
+		} else if isRightBracket(c) {
+			stackLen := len(stack)
+			// must match the bracket on the top
+			if stackLen > 0 && isMatchedBracket(stack[stackLen-1], c) {
+				stack = stack[:stackLen-1]
+			} else {
+				return false
+			}
+		} else {
+			// should not occur this
+		}
+	}
+	// must be zero at last
+	return len(stack) == 0
+}
+
+func isLeftBracket(c int32) bool {
+	if c == '(' || c == '[' || c == '{' {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isRightBracket(c int32) bool {
+	if c == ')' || c == ']' || c == '}' {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isMatchedBracket(c1 int32, c2 int32) bool {
+	return c1 == '(' && c2 == ')' || c1 == '[' && c2 == ']' || c1 == '{' && c2 == '}'
+}
+
+// Second:
+// A better simple solution is remove the match brackets: (),[],{}
+// Time: O(n)
+// Space: O(1)
+// ({{{{}}})) -> ({{{}})) -> ({{})) -> ({)) -> can not remove matched brackets any more.
---
+++ src/string/valid-parentheses_test.go
@@ -0,0 +1,53 @@
+package string
+
+import "testing"
+
+func TestIsValid(t *testing.T) {
+	result := isValid("()")
+	expected := true
+	if result != expected {
+		t.Errorf("expected is [%t], actual is [%t]", expected, result)
+	}
+
+	result = isValid("()[]{}")
+	expected = true
+	if result != expected {
+		t.Errorf("expected is [%t], actual is [%t]", expected, result)
+	}
+
+	result = isValid("(]")
+	expected = false
+	if result != expected {
+		t.Errorf("expected is [%t], actual is [%t]", expected, result)
+	}
+
+	result = isValid("([)]")
+	expected = false
+	if result != expected {
+		t.Errorf("expected is [%t], actual is [%t]", expected, result)
+	}
+
+	result = isValid("{[]}")
+	expected = true
+	if result != expected {
+		t.Errorf("expected is [%t], actual is [%t]", expected, result)
+	}
+
+	result = isValid("({{{{}}}))")
+	expected = false
+	if result != expected {
+		t.Errorf("expected is [%t], actual is [%t]", expected, result)
+	}
+
+	result = isValid("([}}])")
+	expected = false
+	if result != expected {
+		t.Errorf("expected is [%t], actual is [%t]", expected, result)
+	}
+
+	result = isValid("({[)")
+	expected = false
+	if result != expected {
+		t.Errorf("expected is [%t], actual is [%t]", expected, result)
+	}
+}
