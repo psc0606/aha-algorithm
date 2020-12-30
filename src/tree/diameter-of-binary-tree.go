9d99835bc955e6cb11174a537353ecd57f929be6
pengshaocheng <psc0606@163.com>
2020-12-31 00:42:17 +0800

    solved https://leetcode-cn.com/problems/diameter-of-binary-tree/

---
+++ src/tree/diameter-of-binary-tree.go
@@ -0,0 +1,33 @@
+package tree
+
+// https://leetcode-cn.com/problems/diameter-of-binary-tree/
+func diameterOfBinaryTree(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	diameter := 0
+	depth(root, &diameter)
+	return diameter
+}
+
+func depth(root *TreeNode, diameter *int) int {
+	if root == nil {
+		return 0
+	}
+	// left child depth
+	leftChildDepth := depth(root.Left, diameter)
+	// right child depth
+	rightChildDepth := depth(root.Right, diameter)
+
+	// update diameter
+	newDiameter := leftChildDepth + rightChildDepth
+	if *diameter < newDiameter {
+		*diameter = newDiameter
+	}
+
+	if leftChildDepth > rightChildDepth {
+		return leftChildDepth + 1
+	} else {
+		return rightChildDepth + 1
+	}
+}
