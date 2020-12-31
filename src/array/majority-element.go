c6a554a73812bed8cad2e6a5beb32797c1b6bf5c
pengshaocheng <psc0606@163.com>
2020-12-31 23:46:54 +0800

    solved https://leetcode-cn.com/problems/majority-element/

---
+++ src/array/majority-element.go
@@ -0,0 +1,25 @@
+package array
+
+// https://leetcode-cn.com/problems/majority-element/
+// `Boyer–Moore majority vote algorithm` aha-algorithm/paper/A Fast Majority Vote Algorithm.pdf
+// Time: O(n)
+// Space: O(1)
+// example: [3,2,3]
+func majorityElement(nums []int) int {
+	// assert nums not empty
+
+	cand := nums[0]
+	k := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == cand {
+			k++
+		} else {
+			k--
+		}
+		if k == 0 {
+			cand = nums[i]
+			k = 1
+		}
+	}
+	return cand
+}
---
+++ src/array/majority-element_test.go
@@ -0,0 +1,19 @@
+package array
+
+import "testing"
+
+func TestMajorityElement(t *testing.T) {
+	nums := []int{3, 2, 3}
+	res := majorityElement(nums)
+	expected := 3
+	if res != expected {
+		t.Errorf("expected is [%d], actual is [%d]", expected, res)
+	}
+
+	nums = []int{2, 2, 1, 1, 1, 2, 2}
+	res = majorityElement(nums)
+	expected = 2
+	if res != expected {
+		t.Errorf("expected is [%d], actual is [%d]", expected, res)
+	}
+}
