b48041f727bf9fb3bbf2411f31ecc0f8228b6e83
pengshaocheng <psc0606@163.com>
2021-01-01 18:00:03 +0800

    solved https://leetcode-cn.com/problems/single-number/

---
+++ src/array/single-number.go
@@ -0,0 +1,13 @@
+package array
+
+// https://leetcode-cn.com/problems/single-number/
+// Time: O(n)
+// Space: O(1)
+func singleNumber(nums []int) int {
+	// assert at least one elements
+	res := nums[0]
+	for i := 1; i < len(nums); i++ {
+		res ^= nums[i]
+	}
+	return res
+}
