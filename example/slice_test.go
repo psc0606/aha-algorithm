aac1d8f9b194397f3a4561248578457628bb42a0
pengshaocheng <psc0606@163.com>
2021-02-22 23:25:17 +0800

    merge two slice

--- example/slice_test.go
+++ example/slice_test.go
@@ -31,6 +31,14 @@ func TestAppend(t *testing.T) {
 	fmt.Println(slice)
 }
 
+// merge two slice into one
+func TestMergeTwoSlice(t *testing.T) {
+	s1 := []int{1, 2, 3}
+	s2 := []int{4, 5, 6}
+	s1 = append(s1, s2...)
+	fmt.Println(s1)
+}
+
 func TestSlice(t *testing.T) {
 	str := "123456"
 
