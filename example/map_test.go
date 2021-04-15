da2afe08d5b8f9221716f6999672130f267632e5
pengshaocheng <psc0606@163.com>
2021-04-15 22:31:36 +0800

    go map test

---
+++ example/map_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetNotExists(t *testing.T) {
+	ht := make(map[int]bool)
+	v, ok := ht[0]
+	// key not exists, but return default false.
+	if v != false {
+		t.Errorf("expected is [%v], actual is [%v]", false, v)
+	}
+	// ok tell you the element exists or not.
+	if ok != false {
+		t.Errorf("expected is [%v], actual is [%v]", false, v)
+	}
+}
+
+func TestMapIterator(t *testing.T) {
+	ht := make(map[int]int)
+	ht[0] = 0
+	ht[1] = 1
+	for k, v := range ht {
+		fmt.Println(k, v)
+	}
+}
