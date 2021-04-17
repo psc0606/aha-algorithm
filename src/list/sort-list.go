54b55a561add3e4aae938ce2e2cab7b8d384bc97
pengshaocheng <psc0606@163.com>
2021-04-18 01:10:11 +0800

    optimize list merge sort

--- src/list/sort-list.go
+++ src/list/sort-list.go
@@ -17,67 +17,17 @@ func sortList(head *ListNode) *ListNode {
 		return head
 	}
 
-	// find mid of list
-	var findMid func(head *ListNode) *ListNode
-	findMid = func(head *ListNode) *ListNode {
-		// at least two node
-		dummy := &ListNode{Next: head}
-		slow, fast := dummy, dummy
-		for fast != nil && fast.Next != nil {
-			slow = slow.Next
-			fast = fast.Next.Next
-		}
-		return slow
+	dummy := &ListNode{Next: head}
+	slow, fast := dummy, dummy
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
 	}
 
-	// merge two sort list
-	var mergeSortList func(head1 *ListNode, head2 *ListNode) *ListNode
-	mergeSortList = func(head1 *ListNode, head2 *ListNode) *ListNode {
-		if head1 == nil {
-			return head2
-		}
-		if head2 == nil {
-			return head1
-		}
-		var head, tail *ListNode
-		for head1 != nil && head2 != nil {
-			if head1.Val < head2.Val {
-				if head == nil {
-					head = head1
-					tail = head1
-				} else {
-					tail.Next = head1
-					tail = tail.Next
-				}
-				head1 = head1.Next
-			} else {
-				if head == nil {
-					head = head2
-					tail = head2
-				} else {
-					tail.Next = head2
-					tail = tail.Next
-				}
-				head2 = head2.Next
-			}
-		}
-		for ; head1 != nil; head1 = head1.Next {
-			tail.Next = head1
-			tail = tail.Next
-		}
-		for ; head2 != nil; head2 = head2.Next {
-			tail.Next = head2
-			tail = tail.Next
-		}
-		return head
-	}
-
-	mid := findMid(head)
-	left, right := head, mid.Next
-	mid.Next = nil
-	left = sortList(left)
-	right = sortList(right)
-	return mergeSortList(left, right)
+	left, right := head, slow.Next
+	slow.Next = nil
+	left, right = sortList(left), sortList(right)
+	return mergeTwoLists(left, right)
 }
 
 // Merge from down to top.
