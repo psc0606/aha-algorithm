da5e03f8981f42f354a65429bba7c5c1f23fd76d
pengshaocheng <psc0606@163.com>
2021-04-18 01:00:30 +0800

    optimize merge-two-sorted-lists.go

--- src/list/merge-two-sorted-lists.go
+++ src/list/merge-two-sorted-lists.go
@@ -3,42 +3,22 @@ package list
 // https://leetcode-cn.com/problems/merge-two-sorted-lists/
 // https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
 // Time: O(m + n)
-// Space: O(1)
+// We can still use dummy node to simplify the code, or we have to find the head.
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil {
-		return l2
-	}
-	if l2 == nil {
-		return l1
-	}
-
-	head := l1
-	if l2.Val < l1.Val {
-		head = l2
-		l2 = l2.Next
-	} else {
-		l1 = l1.Next
-	}
-	tail := head
+	dummy := new(ListNode)
+	tail := dummy
 	for l1 != nil && l2 != nil {
 		if l1.Val <= l2.Val {
-			tail.Next = l1
-			l1 = l1.Next
+			tail.Next, l1 = l1, l1.Next
 		} else {
-			tail.Next = l2
-			l2 = l2.Next
+			tail.Next, l2 = l2, l2.Next
 		}
 		tail = tail.Next
 	}
-	for l1 != nil {
+	if l1 != nil {
 		tail.Next = l1
-		tail = tail.Next
-		l1 = l1.Next
-	}
-	for l2 != nil {
+	} else {
 		tail.Next = l2
-		tail = tail.Next
-		l2 = l2.Next
 	}
-	return head
+	return dummy.Next
 }
