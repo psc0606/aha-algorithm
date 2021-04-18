d59ac3e39b71420a03785dbfbce73d91e233b222
pengshaocheng <psc0606@163.com>
2021-04-18 12:54:58 +0800

    optimize sort list

--- src/list/sort-list.go
+++ src/list/sort-list.go
@@ -33,7 +33,45 @@ func sortList(head *ListNode) *ListNode {
 // Merge from down to top.
 // Time: O(nlogn)
 // Space: O(1)
-// loop
 func sortListII(head *ListNode) *ListNode {
-	return nil
+	if head == nil || head.Next == nil {
+		return head
+	}
+	// n is the length of list
+	n, p := 0, head
+	for p != nil {
+		n++
+		p = p.Next
+	}
+
+	dummy := &ListNode{Next: head}
+	for step := 1; step <= n; step *= 2 {
+		prev, curr := dummy, dummy
+		for curr != nil { // every sub-list
+			leftHead := prev.Next
+			rightHead := split(prev.Next, step)
+			curr = split(rightHead, step)
+			prev.Next = mergeTwoLists(leftHead, rightHead)
+			for prev.Next != nil {
+				prev = prev.Next
+			}
+			prev.Next = curr
+		}
+	}
+	return dummy.Next
+}
+
+// return right head of split list, the left tail pointer to null.
+func split(head *ListNode, step int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	dummy := &ListNode{Next: head}
+	curr := dummy
+	for i := 0; i < step && curr.Next != nil; i++ {
+		curr = curr.Next
+	}
+	right := curr.Next
+	curr.Next = nil
+	return right
 }
--- src/list/sort-list_test.go
+++ src/list/sort-list_test.go
@@ -40,3 +40,40 @@ func TestSortList(t *testing.T) {
 		t.Errorf("expected is [%v], actual is [%v]", PrintList(expected), PrintList(actual))
 	}
 }
+
+func TestSortListII(t *testing.T) {
+	head := BuildList([]int{5, 2, 1, 4})
+	actual := sortListII(head)
+	expected := BuildList([]int{1, 2, 4, 5})
+	if !TestListEqual(expected, actual) {
+		t.Errorf("expected is [%v], actual is [%v]", PrintList(expected), PrintList(actual))
+	}
+
+	head = BuildList([]int{5})
+	actual = sortListII(head)
+	expected = BuildList([]int{5})
+	if !TestListEqual(expected, actual) {
+		t.Errorf("expected is [%v], actual is [%v]", PrintList(expected), PrintList(actual))
+	}
+
+	head = BuildList([]int{5, 2})
+	actual = sortListII(head)
+	expected = BuildList([]int{2, 5})
+	if !TestListEqual(expected, actual) {
+		t.Errorf("expected is [%v], actual is [%v]", PrintList(expected), PrintList(actual))
+	}
+
+	head = BuildList([]int{5, 2, 1})
+	actual = sortListII(head)
+	expected = BuildList([]int{1, 2, 5})
+	if !TestListEqual(expected, actual) {
+		t.Errorf("expected is [%v], actual is [%v]", PrintList(expected), PrintList(actual))
+	}
+
+	head = BuildList([]int{3, 5, 2, 1})
+	actual = sortListII(head)
+	expected = BuildList([]int{1, 2, 3, 5})
+	if !TestListEqual(expected, actual) {
+		t.Errorf("expected is [%v], actual is [%v]", PrintList(expected), PrintList(actual))
+	}
+}
